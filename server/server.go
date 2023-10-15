95833c93a74b9f7734ecb1c71aeadf3cddd213bd
Dan Cohen <danrcohen@gmail.com>
2023-10-15 00:35:38 -0500

    grpc web server with web client embedded

--- server/server.go
+++ server/server.go
@@ -4,12 +4,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"net"
+	"net/http"
 	"os"
+	"os/signal"
 
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"google.golang.org/grpc"
 
 	pb "github.com/venim/hello-bazel/proto"
+	"github.com/venim/hello-bazel/web/static"
 )
 
 func main() {
@@ -20,25 +25,78 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-	lis, err := net.Listen("tcp", "localhost:10123")
-	if err != nil {
-		return fmt.Errorf("net.Listen(): %w", err)
+	grpcSrv := grpc.NewServer()
+	pb.RegisterHelloServiceServer(grpcSrv, new())
+
+	wrappedSrv := grpcweb.WrapServer(grpcSrv, grpcweb.WithCorsForRegisteredEndpointsOnly(false))
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if wrappedSrv.IsGrpcWebRequest(r) {
+			wrappedSrv.ServeHTTP(w, r)
+			return
+		}
+		http.FileServer(http.FS(static.Static)).ServeHTTP(w, r)
+	})
+	httpServer := &http.Server{Addr: ":8080", Handler: handler}
+	errs := make(chan error, 0)
+	go func() {
+		lis, err := net.Listen("tcp", "localhost:8080")
+		if err != nil {
+			errs <- fmt.Errorf("net.Listen(): %w", err)
+		}
+		defer lis.Close()
+		if err := httpServer.Serve(lis); err != nil {
+			errs <- fmt.Errorf("Serve(): %w", err)
+		}
+	}()
+	go func() {
+		lis, err := net.Listen("tcp", "localhost:10123")
+		if err != nil {
+			errs <- fmt.Errorf("net.Listen(): %w", err)
+		}
+		defer lis.Close()
+		if err := grpcSrv.Serve(lis); err != nil {
+			errs <- fmt.Errorf("Serve(): %w", err)
+		}
+	}()
+	defer func() {
+		httpServer.Shutdown(ctx)
+		grpcSrv.GracefulStop()
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+
+	for {
+		select {
+		case <-stop:
+			slog.Warn("ctrl+c captured! shutting down")
+			return nil
+		case <-ctx.Done():
+			return nil
+		case err := <-errs:
+			return err
+		}
 	}
-	defer lis.Close()
-	srv := grpc.NewServer()
-	pb.RegisterHelloServiceServer(srv, &svc{})
-	if err := srv.Serve(lis); err != nil {
-		return fmt.Errorf("Serve(): %w", err)
-	}
-	return nil
 }
 
 type svc struct {
 	pb.UnimplementedHelloServiceServer
+	logger slog.Logger
 }
 
-func (*svc) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+func new() *svc {
+	return &svc{
+		logger: *slog.New(slog.NewJSONHandler(os.Stdout, nil)),
+	}
+}
+
+func (svc *svc) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponse, error) {
+	name := req.GetName()
+	svc.logger.Info("request recieved",
+		slog.String("method", "SayHello"),
+		slog.String("name", name),
+	)
 	return &pb.HelloResponse{
-		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
+		Message: fmt.Sprintf("Hello, %s!", name),
 	}, nil
 }
--- tools/go_third_party.bzl
+++ tools/go_third_party.bzl
@@ -5,6 +5,52 @@ load("@bazel_gazelle//:deps.bzl", "go_repository")
 def go_deps():
     """Go dependencies"""
     go_repository(
+        name = "com_github_cenkalti_backoff",
+        importpath = "github.com/cenkalti/backoff",
+        sum = "h1:tNowT99t7UNflLxfYYSlKYsBpXdEet03Pg2g16Swow4=",
+        version = "v2.2.1+incompatible",
+    )
+    go_repository(
+        name = "com_github_cenkalti_backoff_v4",
+        importpath = "github.com/cenkalti/backoff/v4",
+        sum = "h1:y4OZtCnogmCPw98Zjyt5a6+QwPLGkiQsYW5oUqylYbM=",
+        version = "v4.2.1",
+    )
+
+    go_repository(
+        name = "com_github_desertbit_timer",
+        importpath = "github.com/desertbit/timer",
+        sum = "h1:U5y3Y5UE0w7amNe7Z5G/twsBW0KEalRQXZzf8ufSh9I=",
+        version = "v0.0.0-20180107155436-c41aec40b27f",
+    )
+
+    go_repository(
+        name = "com_github_improbable_eng_grpc_web",
+        importpath = "github.com/improbable-eng/grpc-web",
+        sum = "h1:BN+7z6uNXZ1tQGcNAuaU1YjsLTApzkjt2tzCixLaUPQ=",
+        version = "v0.15.0",
+    )
+    go_repository(
+        name = "com_github_klauspost_compress",
+        importpath = "github.com/klauspost/compress",
+        sum = "h1:NE3C767s2ak2bweCZo3+rdP4U/HoyVXLv/X9f2gPS5g=",
+        version = "v1.17.1",
+    )
+
+    go_repository(
+        name = "com_github_rs_cors",
+        importpath = "github.com/rs/cors",
+        sum = "h1:L0uuZVXIKlI1SShY2nhFfo44TYvDPQ1w4oFkUJNfhyo=",
+        version = "v1.10.1",
+    )
+    go_repository(
+        name = "io_nhooyr_websocket",
+        importpath = "nhooyr.io/websocket",
+        sum = "h1:usjR2uOr/zjjkVMy0lW+PPohFok7PCow5sDjLgX4P4g=",
+        version = "v1.8.7",
+    )
+
+    go_repository(
         name = "org_golang_google_grpc",
         importpath = "google.golang.org/grpc",
         sum = "h1:BjnpXut1btbtgN/6sp+brB2Kbm2LjNXnidYujAVbSoQ=",
